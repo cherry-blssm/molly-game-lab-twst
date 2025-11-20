5859ccc8cd5eef154b93daa41faf76091df80a34
Molly <kaedeakamatsu2369420@gmail.com>
2025-11-20 14:32:53 -0500

    lab 3 DONEgit add . :3

--- game.py
+++ game.py
@@ -16,12 +16,80 @@ if __name__ == '__main__':
     # any buffs / debuffs?
     # any critical success / failure?
 
-    name = input('Name: ')
-    role = input('Role: ')
+    username = input('Name: ')
 
-    print('Your name is ' + name + ' and your role is ' + role + '.')
-    print_dramatic_text('Our adventure begins in a shady tavern ...')
+    print_dramatic_text ('Welcome, ' + username + '!')
+    answer = input('Please imput your password: ')
+    if answer.lower() == 'love':
+        print_dramatic_text('Correct!!')
+    else:
+        print_dramatic_text('Uh oh!!')
 
-    input('Press Enter to roll a d20.')
-    roll = random.randint(1, 20)
-    draw_d20(roll)
+    answer = input('HINT - Romantic Feeling ')
+    while answer.lower() != 'love':
+        print_dramatic_text('Uh oh ...')
+        answer = input('Try again!! HINT - Romantic Feeling ')
+    print_dramatic_text('Correct!!')
+    answer = input('HINT - Laugh Out Loud!! ')
+    while answer.lower() != 'lol':
+        print_dramatic_text('Uh oh ...')
+        answer = input('Try again!! HINT - Laugh Out Loud!! ')
+    print_dramatic_text('Correct!!')
+    answer = input('HINT - That Number. ')
+    while answer != '67':
+        print_dramatic_text('Uh oh ...')
+        answer = input('Try again!! HINT - That Number. ')
+    print_dramatic_text('Correct!!')
+    answer = input('HINT - Disney princess with hair for days ')
+    while answer.lower() != 'rapunzel':
+        print_dramatic_text('Uh oh ...')
+        answer = input('Try again!! HINT - Disney princess with hair for days ')
+    print_dramatic_text('Correct!!')
+    answer = input('HINT - One of the series on a poster in Mr Mazeys room ')
+    while answer.lower() != 'deathnote':
+        print_dramatic_text('Uh oh ...')
+        answer = input('Try again!! HINT - One of the series on a poster in Mr Mazeys room ')
+    print_dramatic_text('Correct!!')
+    answer = input('HINT - What is the prize for this presentation? ')
+    while answer.lower() != 'labubu':
+        print_dramatic_text('Uh oh ...')
+        answer = input('Try again!! HINT - What is the prize for this presentation? ')
+    print_dramatic_text('Correct!!')
+    answer = input('HINT - How old am I? ')
+    while answer != '15':
+        print_dramatic_text('Uh oh ...')
+        answer = input('Try again!! HINT - How old am I? ')
+    print_dramatic_text('Correct!!')
+    answer = input('HINT - PUT IT ALL TOGETHER!! ')
+    while answer.lower() != 'lovelol67rapunzeldeathnotelabubu15':
+        print_dramatic_text('Uh oh ...')
+        answer = input('Try again!! HINT - Every answer, no spaces!! ')
+    print_dramatic_text('CONGRADULATIONS!!')
+
+    print ("""
+           ⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠁⠀⠀⠀⠙⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠶⠒⠶⠾⠷⠖⠀⠀⠀⠀⠀⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣣⠶⠶⢤⡀⣠⣤⣄⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⣸⢳⡏⠀⠀⠈⠿⠅⠀⠙⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⣤⣄⣀⠀⠀⣼⠏⠘⣧⠀⠀⠀⠀⠀⠀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢾⣷⣦⠀⠈⢷⣄⠀⢀⣠⠾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢧⡀⠀⠀⣹⣿⣏⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⠀⠀⠈⢹⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡆⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⡗⠀⢀⡟⠀⣠⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣦⣄⠀⠀⠀⠀⣸⠃⠀⣼⠁⣼⢨⣵⣦⢢⣀⣀⡀⡀⠀⠀⢠⠞⠋⠙⢳⡄⠀
+⠀⢀⡶⣛⣛⠳⣆⣀⣀⣠⣤⣤⠤⠾⢳⣿⣿⣾⢷⠀⠀⢰⡏⠀⠀⣏⢰⠇⠹⢿⠿⠃⠉⢉⣍⣉⡙⠛⠛⠰⣿⣿⡟⣧⠀
+⠀⢸⡗⣿⣿⡿⠀⣤⣀⣠⡄⠀⠀⠀⠀⠙⠛⠁⢸⡄⢠⡏⠀⠀⠀⢿⡿⠀⠀⠀⠀⠀⠀⠀⠙⠛⠁⠀⠀⠀⠈⠁⠀⣿⠀
+⠀⠀⡇⠀⠀⠀⠀⠛⠥⠖⠃⠀⠀⠀⠀⠀⠀⠀⠀⠛⣿⣤⡀⠀⠀⢸⡟⣦⡴⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⢿⠀
+⣠⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣟⠁⠀⠀⣾⠛⠋⢀⣟⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡏⠹⣆⢸⡀
+⢿⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡇⠘⠧⠀⠀⣿⠀⠀⠛⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠘⠷⠀⠀⢸⡇
+⠠⠑⠼⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠁⠟⠃⠐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⢸⡇⠀⠀⢰⡇⠀⠀⢀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⣯⠀⠀⠀⣠⣤⠴⠶⠶⠶⠾⠛⠛⠋⠉⠙⠳⠤⠞⠃⠀⠀⠈⠻⠶⠞⠋⠉⠉⠉⠉⠉⠉⠉⠙⠛⠛⢷⡆⠀⢀⣼⠃
+⠀⠀⠑⠲⠠⠜⠁⠤⠀⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠳⠖⠋⠁⠀
+           """)
+
+    print ('THANKS FOR PLAYING!!')
